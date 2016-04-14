770a9f6872d93ddfca87c605827b3c866f3541f3
metoray <metoray@gmail.com>
2016-04-14 13:07:15 +0200

    Made connection interface!

---
+++ src/knof/controllers/ConnectionController.java
@@ -0,0 +1,22 @@
+package knof.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ConnectionController {
+
+    @FXML
+    TextField hostName;
+
+    @FXML
+    TextField portNumber;
+
+    @FXML
+    TextField userName;
+
+    @FXML
+    public void connect() {
+
+    }
+
+}
