4e0489b2122396d375a53f2e4165637242e716c2
metoray <metoray@gmail.com>
2016-04-17 15:21:38 +0200

    Made command formatting system

---
+++ src/knof/command/Command.java
@@ -0,0 +1,53 @@
+package knof.command;
+
+public enum Command {
+    LOGIN("login", new ArgumentField(String.class)),
+    GET_PLAYERLIST("get playerlist"),
+    GET_GAMELIST("get gamelist"),
+    CHALLENGE("challenge", new ArgumentField(String.class, true));
+
+    private final static IFormatter quotedFormatter = (Object obj) -> "\"" + obj + "\"";
+    private final ArgumentField[] fields;
+    private final String prefix;
+
+    Command(String prefix, ArgumentField... fields) {
+        this.prefix = prefix;
+        this.fields = fields;
+    }
+
+    static class ArgumentField {
+        private final IFormatter formatter;
+        private final Class fieldClass;
+
+        public ArgumentField(Class fieldClass) {
+            this(fieldClass, false);
+        }
+
+        public ArgumentField(Class<String> fieldClass, boolean quoted) {
+            this.fieldClass = fieldClass;
+            this.formatter = quoted?quotedFormatter:Object::toString;
+        }
+
+        public String format(Object obj) {
+            return this.formatter.format(obj);
+        }
+    }
+
+    public String format(Object... arguments) {
+        if(this.fields.length==arguments.length) {
+            String message = this.prefix;
+            for(int i=0; i<this.fields.length; i++) {
+                message += " " + this.fields[0].format(arguments[i]);
+            }
+            return message;
+        }
+        System.err.println("Wrong number of arguments to command!");
+        return ""; // Do nothing then?
+    }
+
+    @FunctionalInterface
+    private interface IFormatter {
+        String format(Object obj);
+    }
+
+}
