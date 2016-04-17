89658da666e1c0c4304f0f5334e91a8e15ebc74e
metoray <metoray@gmail.com>
2016-04-17 19:44:10 +0200

    Fixed potential blocking vs nonblocking bug

--- src/knof/connection/CommandHandler.java
+++ src/knof/connection/CommandHandler.java
@@ -6,15 +6,13 @@ import knof.event.EventSystem;
 import knof.event.events.StatusEvent;
 
 import java.io.PrintWriter;
-import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class CommandHandler {
     private final BlockingQueue<StatusEvent> eventQueue = new LinkedBlockingQueue<>();
     private final PrintWriter out;
-    private int discard;
+    private final BlockingQueue<Boolean> discardQueue = new LinkedBlockingQueue<>();
 
     public CommandHandler(EventSystem eventSystem, PrintWriter out) {
         this.out = out;
@@ -23,40 +21,38 @@ public class CommandHandler {
 
     public StatusEvent sendCommand(Command command, boolean blocking, Object... arguments) {
         this.out.println(command.format(arguments));
-        if(blocking) {
-            try {
-                StatusEvent ev = eventQueue.take();
-                return ev;
-            } catch (InterruptedException e) {
-                return new StatusEvent.Error("CLIENT: " + e.getMessage());
+        try {
+            if (blocking) {
+                    discardQueue.put(false);
+                    StatusEvent ev = eventQueue.take();
+                    return ev;
+            } else {
+                discardQueue.put(true);
+                return null;
             }
-        }
-        else {
-            synchronized (this) {
-                discard++;
-            }
-            return null;
+        } catch (InterruptedException e) {
+            return new StatusEvent.Error("CLIENT: " + e.getMessage());
         }
     }
 
     public void discard(int n) {
-        synchronized (this) {
-            discard += n;
+        for (int i=0; i<n; i++) {
+            try {
+                discardQueue.put(true);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @EventHandler
     public void onStatus(StatusEvent ev) {
-        synchronized (this) {
-            if (discard > 0) {
-                discard--;
-            } else {
-                try {
-                    eventQueue.put(ev);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        try {
+            if (!discardQueue.take()) {
+                eventQueue.put(ev);
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
