f19e7e7e2e55148362ae61ef0044d7d0b2d9b70c
Thijsku <thijs.kuilman@gmail.com>
2016-04-19 14:27:47 +0200

    Changed name of command

--- src/knoftests/event/EventSystemTest.java
+++ src/knoftests/event/EventSystemTest.java
@@ -91,7 +91,7 @@ public class EventSystemTest {
     @Test
     public void testPlayerlist() {
         EventSystem es = new EventSystem();
-        IEvent ev = es.parse("SVR GAMELIST [\"<player1>\"]");
+        IEvent ev = es.parse("SVR PLAYERLIST [\"<player1>\"]");
         assertEquals(ev, ListEvent.Players.class);
         ListEvent.Players playerlist = (ListEvent.Players) ev;
         List<String> checklist = Arrays.asList("player1");
