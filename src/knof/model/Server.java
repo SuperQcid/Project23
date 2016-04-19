a925b7bc28f2020a42a5d17599e78b4f92a47332
vincentb <v.r.baalmans@st.hanze.nl>
2016-04-19 13:30:31 +0200

    Can now display the games

--- src/knof/model/Server.java
+++ src/knof/model/Server.java
@@ -18,49 +18,52 @@ import java.util.TimerTask;
  * A model representing a server that has been connected to
  */
 public class Server {
-    public final Connection connection;
-    public final ObservableList<String> games;
-    public final ObservableList<String> players;
-    public final ObservableList<Challenge> challenges;
+	public final Connection connection;
+	public final ObservableList<String> games;
+	public final ObservableList<String> players;
+	public final ObservableList<Challenge> challenges;
 
-    private final Timer timer = new Timer(true);
+	private final Timer timer = new Timer(true);
 
-    public Server(Connection connection) {
-        this.connection = connection;
+	public Server(Connection connection) {
+		this.connection = connection;
 
-        ObservableList<String> gameList = FXCollections.observableArrayList();
-        this.games = FXCollections.synchronizedObservableList(gameList);
+		ObservableList<String> gameList = FXCollections.observableArrayList();
+		this.games = FXCollections.synchronizedObservableList(gameList);
 
-        ObservableList<String> playerList = FXCollections.observableArrayList();
-        this.players = FXCollections.synchronizedObservableList(playerList);
+		ObservableList<String> playerList = FXCollections.observableArrayList();
+		this.players = FXCollections.synchronizedObservableList(playerList);
 
-        ObservableList<Challenge> chalengeList = FXCollections.observableArrayList();
-        this.challenges = FXCollections.synchronizedObservableList(chalengeList);
+		ObservableList<Challenge> chalengeList = FXCollections.observableArrayList();
+		this.challenges = FXCollections.synchronizedObservableList(chalengeList);
 
-        this.connection.eventSystem.register(this);
+		this.connection.eventSystem.register(this);
 
-        this.timer.scheduleAtFixedRate(new CommandTask(connection, this, Command.GET_PLAYERLIST), 0, 4000);
-    }
+		this.timer.scheduleAtFixedRate(new CommandTask(connection, this, Command.GET_PLAYERLIST), 0, 4000);
+		this.timer.scheduleAtFixedRate(new CommandTask(connection, this, Command.GET_GAMELIST), 0, 4000);
+	}
 
-    @EventHandler
-    public void onPlayerList(ListEvent.Players event) {
-        Platform.runLater(() -> {
-            System.out.println(event);
-            this.players.removeIf((String player) -> !event.contains(player));
-            event.removeIf(this.players::contains);
+	@EventHandler
+	public void onPlayerList(ListEvent.Players event) {
+		Platform.runLater(() -> {
+			System.out.println(event);
+			this.players.removeIf((String player) -> !event.contains(player));
+			event.removeIf(this.players::contains);
 
-            this.players.addAll(event);
-            System.out.println(this.players);
-        });
-    }
-    
-    @EventHandler
-    public void onGameList(ListEvent.Games event) {
-    	System.out.println(event);
-        this.games.removeIf((String game) -> !event.contains(game));
-        event.removeIf(this.games::contains);
+			this.players.addAll(event);
+			System.out.println(this.players);
+		});
+	}
 
-        this.games.addAll(event);
-        System.out.println(this.games);
-    }
+	@EventHandler
+	public void onGameList(ListEvent.Games event) {
+		Platform.runLater(() -> {
+			System.out.println(event);
+			this.games.removeIf((String game) -> !event.contains(game));
+			event.removeIf(this.games::contains);
+
+			this.games.addAll(event);
+			System.out.println(this.games);
+		});
+	}
 }
