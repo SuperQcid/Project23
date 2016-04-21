49c3af202c99f82b19a117076d72b3593d1c63bb
vincentb <v.r.baalmans@st.hanze.nl>
2016-04-21 13:23:07 +0200

    When a match starts the popup will now close

--- src/knof/controllers/PopupController.java
+++ src/knof/controllers/PopupController.java
@@ -6,6 +6,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import knof.command.Command;
+import knof.event.EventHandler;
+import knof.event.events.MatchEvent;
 import knof.model.Server;
 
 public class PopupController {
@@ -32,12 +34,19 @@ public class PopupController {
 	    
 	}
 	
+	@EventHandler(later = true)
+	public void closePopupOnGameStart(MatchEvent match){
+		Stage stage = (Stage) cancel.getScene().getWindow();
+	    stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+	}
+	
 	public void addGameToText(String game){
 		this.text.setText(this.text.getText() + " " + game);
 	}
 	
 	public void setServer(Server server){
 		this.server = server;
+		this.server.connection.eventSystem.register(this);
 	}
 	
 	private boolean isServerSet(){
