aeb2a95ed1a0045fc8bfae3d15700d7a4e6759dc
SuperDicq <ricardoprudon@gmail.com>
2016-04-28 13:28:47 +0200

    Added margin to ChoiceBox and added notes to GameController

--- src/knof/controllers/listcell/controllers/GameController.java
+++ src/knof/controllers/listcell/controllers/GameController.java
@@ -2,14 +2,27 @@ package knof.controllers.listcell.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class GameController extends ListCellController {
+	
+	@FXML
+	public ChoiceBox ai;
+	
+	public GameController() {
+		// TODO
+		// Load available AIs from plugin
+		// Add available AIs to ChoiceBox ai.
+	}
     
     @FXML
     public void onButton(ActionEvent e) {
         loadingSign.setVisible(true);
         button.setVisible(false);
         button.setDisable(true);
+        ai.setDisable(true);
+        // TODO
+        // Launch the game with selected AI.
     }
     
     @Override
