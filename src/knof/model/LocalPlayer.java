2c0e3a485193e742d013324568d53ec8a36d72fa
Henk Dieter <henkdieter@gmail.com>
2016-05-03 13:39:37 +0200

    Updated LocalPlayer, removed guess module

--- src/knof/model/LocalPlayer.java
+++ src/knof/model/LocalPlayer.java
@@ -6,8 +6,29 @@ import knof.gamelogic.Side;
 /**
  * Created by Henk Dieter Oordt on 3-5-2016.
  */
-public abstract class LocalPlayer extends Player{
-    public LocalPlayer(String name, Side side, Connection connection) {
+public abstract class LocalPlayer extends Player {
+    protected boolean isAI;
+    /**
+     * Game, used to get the current representation in order to calculate a move
+     */
+    protected Game game;
+
+    public LocalPlayer(String name, Side side, Connection connection, Game game) {
         super(name, side, connection);
+        this.game = game;
     }
+
+    public void setAI(boolean isAI){
+        this.isAI = isAI;
+    }
+
+    public boolean getAI(){
+        return isAI;
+    }
+
+    /**
+     * Returns an AI move
+     * @return
+     */
+    public abstract int getAIMove();
 }
