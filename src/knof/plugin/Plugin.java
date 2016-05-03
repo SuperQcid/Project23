0daa9169d6b20f9468ccefbf77a216b745217e2e
metoray <metoray@gmail.com>
2016-05-03 22:33:23 +0200

    Added player type list to plugin requirements

--- src/knof/plugin/Plugin.java
+++ src/knof/plugin/Plugin.java
@@ -2,12 +2,58 @@ package knof.plugin;
 
 import knof.connection.Connection;
 import knof.model.game.Game;
+import knof.model.game.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Interface for game plugins.
  */
 public abstract class Plugin {
+    protected HashMap<String, PlayerFactory> playerTypes = new HashMap<>();
+
     public abstract String getGameName();
 
     public abstract Game createGame(String playerOneName, String playerTwoName, boolean playerOneIsLocal, Connection connection);
+
+    /**
+     * Return a list of names possible AI players
+     * @return list of names
+     */
+    public Set<String> getPlayerTypes(boolean includeHumanPlayer) {
+        Set<String> typeNames = playerTypes.keySet();
+        if(!includeHumanPlayer) {
+            typeNames.remove(getPlayerTypeName());
+        }
+        return typeNames;
+    }
+
+    /**
+     * Type name of player type
+     * @return type name
+     */
+    public abstract String getPlayerTypeName();
+
+    /**
+     * Create player
+     * @param type type of player
+     * @param connection connection to server
+     * @param game game to play
+     * @param playerName username of player
+     * @return player
+     */
+    public Player createPlayer(String type, Connection connection, Game game, String playerName) {
+        PlayerFactory factory = playerTypes.get(type);
+        if(factory==null) return null;
+
+        return factory.createPlayer(connection, game, playerName);
+    }
+
+    @FunctionalInterface
+    public interface PlayerFactory {
+        Player createPlayer(Connection connection, Game game, String playerName, Object... options);
+    }
 }
