a4d2e64952e28713d4f865b4f0136128c23d9d78
SuperDicq <ricardoprudon@gmail.com>
2016-05-12 12:55:32 +0200

    getPluginList tested and confirmed to work in ChallengePopup

--- src/knof/app/KnofApplication.java
+++ src/knof/app/KnofApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import knof.connection.Connection;
+import knof.model.game.Game;
 import knof.plugin.Plugin;
 import knof.plugin.PluginLoader;
 
