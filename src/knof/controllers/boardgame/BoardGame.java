6fa3f735711b98645c9c74d58a78afa5c8677d5c
SuperDicq <ricardoprudon@gmail.com>
2016-05-12 14:08:22 +0200

    Started on BoardGame class

---
+++ src/knof/controllers/boardgame/BoardGame.java
@@ -0,0 +1,62 @@
+package knof.controllers.boardgame;
+
+import javafx.scene.canvas.Canvas;
+
+public class BoardGame extends Canvas {
+
+	public BoardGame() {
+		
+	}
+	
+	/**
+	 * Gets the row the canvas X coordinate is in.
+	 * @param x
+	 * @return row
+	 */
+	private int getRow(Double x) {
+		return (int) Math.floor(x.intValue() / getCellWidth());
+	}
+	
+	/**
+	 * Gets the column the canvas Y coordinate is in.
+	 * @param y
+	 * @return col
+	 */
+	private int getCol(Double y) {
+		return (int) Math.floor(y.intValue() / getCellHeight());
+	}   
+	
+    /**
+     * Gets the X coordinate of board row on canvas.
+     * @param row
+     * @return x
+     */
+    private double getRowX(int row) {
+    	return getCellWidth() * row;
+    }
+    
+    /**
+     * Gets the Y coordinate of board column on canvas.
+     * @param col
+     * @return y
+     */
+    private double getColY(int col) {
+    	return getCellHeight() * col;
+    }
+    
+    /**
+     * Gets the width of a single cell.
+     * @return width
+     */
+    private double getCellWidth() {
+    	return getWidth() / board.width;
+    }
+    
+    /**
+     * gets the height of a single cell.
+     * @return height
+     */
+    private double getCellHeight() {
+    	return getHeight() / board.height;
+    }
+}
