d225517d4d401a3c7af0545f1144a2819ceec8ec
SuperDicq <ricardoprudon@gmail.com>
2016-05-12 15:27:00 +0200

    Added DrawPiece

--- TicTacToe/src/game/tictactoe/BoardGame.java
+++ TicTacToe/src/game/tictactoe/BoardGame.java
@@ -1,15 +1,93 @@
 package game.tictactoe;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import knof.gamelogic.Piece;
 import knof.gamelogic.Board;
+import knof.gamelogic.Board.Pos;
 
 public class BoardGame extends Canvas {
 	
 	private TicTacToeGame game;
+	public Color colorPlayerOne;
+	public Color colorPlayerTwo;
+	public Pieces pieceType;
+    public double piecePadding = 4;
+    public double pieceLineWidth = 10;
+    public double gridLineWidth = 1;
 	
 	public void setGame(TicTacToeGame game) {
 		this.game = game;
 	}
+	
+    /**
+     * Draws a piece in the given position based on the board.
+     * @param move 
+     */
+	private void drawPiece(Pos pos, Piece piece) {
+    	GraphicsContext gc = getGraphicsContext2D();
+    	switch(piece.getSide()) {
+			case "BLACK":
+				gc.setFill(colorPlayerOne);
+				gc.setStroke(colorPlayerOne);
+				break;
+			case "WHITE":
+				gc.setFill(colorPlayerTwo);
+				gc.setStroke(colorPlayerTwo);
+				break;
+			default:
+				return;
+    	}
+    	switch(pieceType) {
+			case CIRCLES:
+				gc.fillOval(
+					getRowX(pos.x) + piecePadding,
+					getColY(pos.y) + piecePadding,
+					getCellWidth() - 2 * piecePadding,
+					getCellHeight() - 2 * piecePadding
+				);
+				break;
+			case SQAURES:
+				gc.fillRect(
+					getRowX(pos.x) + piecePadding,
+					getColY(pos.y) + piecePadding,
+					getCellWidth() - 2 * piecePadding,
+					getCellHeight() - 2 * piecePadding
+				);
+				break;
+			case XO:
+				gc.setLineWidth(pieceLineWidth);
+				double padding = pieceLineWidth / 2 + piecePadding;
+				switch(piece.getSide()) {
+					case "BLACK":
+						gc.strokeLine(
+							getRowX(pos.x) + padding,
+							getColY(pos.y) + padding,
+							getRowX(pos.x) + getCellWidth() - padding, 
+							getColY(pos.y) + getCellHeight() - padding
+						);
+						gc.strokeLine(
+							getRowX(pos.x) + padding,
+							getColY(pos.y) + getCellHeight() - padding,
+							getRowX(pos.x) + getCellWidth() - padding,
+							getColY(pos.y) + padding
+						);
+						break;
+					case "WHITE":
+						gc.strokeOval(
+							getRowX(pos.x) + padding,
+							getColY(pos.y) + padding,
+							getCellWidth() - 2 * padding,
+							getCellHeight() - 2 * padding
+						);
+						break;
+					default:
+						return;
+				}
+				break;
+    	}
+    }
 	
 	/**
 	 * Gets the row the canvas X coordinate is in.
@@ -61,5 +139,9 @@ public class BoardGame extends Canvas {
      */
     private double getCellHeight() {
     	return getHeight() / game.board.height;
+    }
+    
+    public enum Pieces {
+    	SQAURES, CIRCLES, XO
     }
 }
