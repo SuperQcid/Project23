696b4acf6222f1d99079f08bd1063ce8088e6b4e
Henk Dieter Oordt <henkdieter@gmail.com>
2016-05-15 09:14:59 +0000

    Update ServerController.java

--- KNOF/src/knof/controllers/ServerController.java
+++ KNOF/src/knof/controllers/ServerController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
