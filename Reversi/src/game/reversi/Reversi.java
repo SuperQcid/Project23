3abc99545cf721ca4fc5e5d9d8ca86d019cd4ee0
metoray <metoray@gmail.com>
2016-05-15 16:29:27 +0200

    Implemented reversi

--- Reversi/src/game/reversi/Reversi.java
+++ Reversi/src/game/reversi/Reversi.java
@@ -1,13 +1,23 @@
 package game.reversi;
 
 import knof.connection.Connection;
+import knof.gamelogic.HumanGridGamePlayer;
 import knof.model.game.Game;
 import knof.model.game.HumanPlayer;
 import knof.plugin.Plugin;
 
 public class Reversi extends Plugin {
     public Reversi() {
-        playerTypes.put("human", (connection, game, side, playerName, options) -> new HumanPlayer(playerName, side, connection, game));
+        playerTypes.put(
+                "human",
+                (connection, game, side, playerName, options) -> new HumanGridGamePlayer<>(
+                        playerName,
+                        side,
+                        connection,
+                        (ReversiGame)game,
+                        true
+                )
+        );
     }
 
     @Override
